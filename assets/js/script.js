480745e6c32b0698acf571e3b2e3ea60f086b05c
NileshPatel83 <hselin_83@yahoo.com>
2023-01-04 07:36:55 +1000

    Index.HTML file and folder structure created.

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+var startButton = document.getElementById("#start-button");
+var resetButton = document.getElementById("#reset-button");
+var value = document.getElementById("#value");
+var wins = document.getElementById("#wins");
+var losses = document.getElementById("#losses");
+
+startButton.addEventListener("click", function(){
+    // Start the timer.
+
+
+    // Display random word.
+});
+
