11bd4ed0b58f500b55697a5a9784fd1f05bf7455
NileshPatel83 <hselin_83@yahoo.com>
2023-01-04 15:38:38 +1000

    Day names changed to month names.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,61 +1,87 @@
-//Global constants
-var timeAllowed = 20;
-const daysNames = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+//Constants
+const timeAllowed = 20;
+const blankSpace = "_";
+const monthsNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
+//Global variables.
+
+//Keeps track of time remaning. This is to stop key press event after the remaining time is 0.
+var timeRemaining = timeAllowed;
+
+//Get elements from index file.
 var startButton = document.getElementById("start-button");
 var resetButton = document.getElementById("reset-button");
-var dayName = document.getElementById("day-name");
+var monthName = document.getElementById("month-name");
 var winCount = document.getElementById("wins");
 var lossCount = document.getElementById("losses");
 var timeLeft = document.getElementById("time-left");
 
+//Event listener for start button.
 startButton.addEventListener("click", function(){
 
     //Sets the initial value of allowed time.
     timeLeft.textContent = timeAllowed;
 
-    // Start the timer.
+    // Starts the timer.
     // startTimer();
 
-    //Gets the day name randomly.
-    var dayName = daysNames[Math.floor(Math.random() * daysNames.length)];
-    // displayMessage(dayName);
+    //Gets the month name randomly.
+    var monthName = monthsNames[Math.floor(Math.random() * monthsNames.length)];
 
-    var blankDayName = getBlankDayName(dayName);
-    // displayMessage(blankDayName);
+    //Gets array of blank space position from month name.
+    //Letter in month name will be replaced by blank at these positions.
+    var blankSpacePosition = getBlankSpacePositions(monthName);
+
+    var blankMonthName = getBlankMonthName(monthName, blankSpacePosition);
+    // displayMessage(blankMonthName);
 });
 
-function getBlankDayName(dayName){
+function getBlankMonthName(monthName, blankSpacePosition){
 
     
     
 
 
 
-    return dayName;
+    return monthName;
+}
+
+//Gets array of blank space position from month name.
+//Gets half blank spaces from month name as whole number.
+//Letter in month name will be replaced by blank at these positions.
+function getBlankSpacePositions(monthName){
+    var blankSpacePositions = [];
+
+    //Gets round down half of month length as whole number.
+    //If month is "January" (total 7 letters, so half is 3.5), returns 3.
+    var totalBlankSpaces = Math.floor(monthName.length/2);
+    
+
+    
+    return blankSpacePositions;
 }
 
 //Starts the timer function.
 function startTimer(){
-    var time = timeAllowed;
+    timeRemaining = timeAllowed;
     
     var timeInterval = setInterval(function () {
-        time--;
+        timeRemaining--;
 
         //Sets the new time value.
-        timeLeft.textContent = time;
+        timeLeft.textContent = timeRemaining;
 
         //Stops the timer and displays message that game is over.
-        if(time === 0){
+        if(timeRemaining === 0){
             clearInterval(timeInterval);
 
-            //Displays specified message as day name.
+            //Displays specified message as month name.
             displayMessage("Game Over");
         }
       }, 1000);    
 }
 
-//Function to display specified message as day name.
+//Function to display specified message as month name.
 function displayMessage(message){
-    dayName.textContent = message;
+    monthName.textContent = message;
 }
